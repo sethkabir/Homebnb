127f6371de095b823dfa3047434a930af0017f1f
Kabir <seth.kabir14@gmail.com>
2022-11-28 16:06:08 +0530

    add responsive design

--- src/Component/Filters.tsx
+++ src/Component/Filters.tsx
@@ -1,4 +1,4 @@
-import { IoKeyOutline } from "react-icons/io5"; //new
+import { IoKeyOutline, IoSnowOutline } from "react-icons/io5"; //new
 import { CiMountain1 } from "react-icons/ci"; //top of the world
 import { AiOutlineFire } from "react-icons/ai"; //trending
 import { TbDisabled2, TbBuildingCastle, TbBeach } from "react-icons/tb"; // adapted //castles //beach front
@@ -7,9 +7,10 @@ import {
 	GiFarmTractor,
 	GiIsland,
 	GiCampingTent,
+	GiSurfBoard,
 } from "react-icons/gi"; //play //farms //islands //camping
 import { RiAncientGateLine } from "react-icons/ri"; // hanoks
-import { MdOutlineBedroomParent, MdCabin } from "react-icons/md"; //private rooms //cabins
+import { MdOutlineBedroomParent, MdCabin, MdOutlinePark } from "react-icons/md"; //private rooms //cabins
 import { FaSwimmingPool } from "react-icons/fa"; //swimming pools
 import Filter from "./Filter";
 
@@ -24,20 +25,22 @@ const Filters = () => {
 		{ title: "Top of the world", icon: <CiMountain1 /> },
 		{ title: "Trending", icon: <AiOutlineFire /> },
 		{ title: "Adapted", icon: <TbDisabled2 /> },
-		// { title: "Play", icon: <GiBowlingStrike /> },
-		// { title: "Hanoks", icon: <RiAncientGateLine /> },
-		// { title: "Private rooms", icon: <MdOutlineBedroomParent /> },
-		// { title: "Farms", icon: <GiFarmTractor /> },
-		// { title: "Cabins", icon: <MdCabin /> },
-		// { title: "Swimming pools", icon: <FaSwimmingPool /> },
-		// { title: "Castles", icon: <TbBuildingCastle /> },
-		// { title: "Beach front", icon: <TbBeach /> },
-		// { title: "Islands", icon: <GiIsland /> },
-		// { title: "Camping", icon: <GiCampingTent /> },
+		{ title: "Play", icon: <GiBowlingStrike /> },
+		{ title: "Hanoks", icon: <RiAncientGateLine /> },
+		{ title: "Private rooms", icon: <MdOutlineBedroomParent /> },
+		{ title: "Farms", icon: <GiFarmTractor /> },
+		{ title: "Cabins", icon: <MdCabin /> },
+		{ title: "Swimming pools", icon: <FaSwimmingPool /> },
+		{ title: "Castles", icon: <TbBuildingCastle /> },
+		{ title: "Beach front", icon: <TbBeach /> },
+		{ title: "Islands", icon: <GiIsland /> },
+		{ title: "Camping", icon: <GiCampingTent /> },
+		{ title: "Parks", icon: <MdOutlinePark /> },
+		{ title: "Surfing", icon: <GiSurfBoard /> },
 	];
 	return (
-		<div className="sm:mx-6 md:mx-10 lg:mx-12">
-			<div className="flex justify-start gap-3 sm:gap-4 mt-4 px-3">
+		<div className="sm:mx-6 md:mx-10 lg:mx-12 ">
+			<div className="flex flex-nowrap justify-start gap-2 sm:gap-4 mt-4 px-3 overflow-auto">
 				{sorting.map((obj) => (
 					<Filter {...obj} />
 				))}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const { default: plugin } = require('tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,3 +11,14 @@ module.exports = {
   },
   plugins: [],
 }
+
+// /* Hide scrollbar for Chrome, Safari and Opera */
+// .no-scrollbar::-webkit-scrollbar {
+//   display: none;
+// }
+
+// /* Hide scrollbar for IE, Edge and Firefox */
+// .no-scrollbar {
+//   -ms-overflow-style: none;  /* IE and Edge */
+//   scrollbar-width: none;  /* Firefox */
+// }
