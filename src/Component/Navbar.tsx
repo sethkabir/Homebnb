fd2eb26bcfe115096f04b9331d2e53b6d10a193d
Kabir <seth.kabir14@gmail.com>
2022-11-28 10:55:30 +0530

    add navbar right section

--- src/Component/Navbar.tsx
+++ src/Component/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 	return (
 		<div className="flex justify-between items-center border-b">
 			{/* Left */}
-			<div className=" flex h-20 w-36">
+			<div className=" flex h-20 w-36 ml-4">
 				<img src={logo} className="object-cover -my-10" alt="logo" />
 			</div>
 			{/* Middle */}
@@ -32,12 +32,12 @@ const Navbar = () => {
 				</div>
 			</div>
 			{/* Right */}
-			<div className="flex items-center pr-3">
-				<p>Rent House</p>
-				<TbWorld />
-				<div className="flex items-center">
-					<BiMenu />
-					<FaUserCircle />
+			<div className="flex items-center pr-3 font-semibold text-slate-600">
+				<p className="text-[17px]">Rent House</p>
+				<TbWorld className="mx-4" />
+				<div className="bg-[#ff3f62] flex items-center border px-3 py-2 rounded-full gap-2 text-white font-bold shadow-lg shadow-slate-300 hover:bg-[#ff3f62]/90 duration-200">
+					<BiMenu className="text-[22px] " />
+					<FaUserCircle className="text-[22px] " />
 				</div>
 			</div>
 		</div>
